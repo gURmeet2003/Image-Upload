9ef5fe423dd018cc17319c4cd20a14681e3997ab
Gurmeet Raj <126115742+gURmeet2003@users.noreply.github.com>
2024-02-13 15:59:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [file, setFile] = useState();
-  const [image, setImage] = useState();
+  const [file, setFile] = useState(null);
+  const [image, setImage] = useState("");
 
   const handleUpload = (e) => {
-    const formdata = new FormData();
-    formdata.append("file", file);
-    axios
-      .post("https://image-upload-wcls.vercel.app/upload", formdata)
-      .then((res) => console.log(res))
-      .catch((e) => console.log(e));
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      axios.post("https://image-upload-wcls.vercel.app/upload", formData)
+        .then((res) => console.log(res))
+        .catch((error) => console.error("Upload Error:", error));
+    } else {
+      console.error("No file selected for upload.");
+    }
   };
 
   useEffect(() => {
-    axios
-      .get("https://image-upload-wcls.vercel.app/getImage")
-      .then((res) => setImage(res.data[0].image))
-      .catch((e) => console.log(e));
+    axios.get("https://image-upload-wcls.vercel.app/getImage")
+      .then((res) => {
+        if (res.data.length > 0) {
+          setImage(res.data[0].image);
+        } else {
+          console.error("No image found.");
+        }
+      })
+      .catch((error) => console.error("Fetch Image Error:", error));
   }, []);
 
   return (
@@ -26,7 +34,7 @@ function App() {
       <input type="file" onChange={(e) => setFile(e.target.files[0])} />
       <button onClick={handleUpload}>Upload</button>
       <br />
-      <img src={"https://image-upload-wcls.vercel.app/Images/" + image} alt="" />
+      {image && <img src={"https://image-upload-wcls.vercel.app/Images/" + image} alt="" />}
     </div>
   );
 }
